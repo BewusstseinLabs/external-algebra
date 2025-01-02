9bea14c0a91e75f747275e85e70ee0fc201bc8e4
Shenmarukai <shanemulc@comcast.net>
2025-01-01 23:23:20 -0500

    initial exterior product implementation.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,8 @@
+// Copyright 2024 Bewusstsein Labs
+
+#![allow(incomplete_features)]
+#![feature(adt_const_params)]
+#![feature(generic_const_exprs)]
 
 pub mod ops;
 pub mod vector;
--- rust/src/vector.rs
+++ rust/src/vector.rs
@@ -1,5 +1,11 @@
+// Copyright 2024 Bewusstsein Labs
 
-use linear_algebra::vector::Vector;
+use std::ops::{ Sub, Mul };
+
+use linear_algebra::{
+    vector::Vector,
+    ops::TensorProduct
+};
 
 use crate::ops::{
     InteriorProduct,
@@ -17,15 +23,32 @@ where
 
     }
 }
+*/
 
-impl<T, const COL: usize> ExteriorProduct for Vector<T, COL>
+impl<T, const LHS_COL: usize, const RHS_COL: usize> ExteriorProduct<Vector<T, RHS_COL>> for Vector<T, LHS_COL>
 where
-    T: Default + Debug + Copy
+    T: Default + std::fmt::Debug + Copy + Sub<Output = T> + Mul<Output = T>,
+    [(); LHS_COL * RHS_COL]:,
+    [(); RHS_COL * LHS_COL]:
 {
-    type Output;
+    type Output = Vector<T, {LHS_COL * RHS_COL}>;
 
-    fn exterior_product( self, rhs: Self ) -> Self::Output {
-
+    fn exterior_product( self, rhs: Vector<T, RHS_COL> ) -> Self::Output {
+        self.tensor_product( rhs ) - rhs.tensor_product( self )
     }
 }
-*/
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exterior_product() {
+        let lhs = Vector::<f64, 3>::new( [4.0, 5.0, 6.0] );
+        let rhs = Vector::<f64, 3>::new( [4.0, 5.0, 6.0] );
+
+        let result = lhs.exterior_product( rhs );
+
+        assert_eq!( result, Vector::<f64, 9>::new( [ -3.0, 6.0, -3.0, 6.0, -12.0, 6.0, -3.0, 6.0, -3.0 ] ) );
+    }
+}
